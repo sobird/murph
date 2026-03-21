9b0ca4ea465b00840cced6aec6e3cab5e1053c13
sobird <i@sobird.me>
2026-03-21 19:47:25 +0800

    feat: using tsup instead of Rollup for bundling

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: [
+    'src/index.ts',
+  ],
+  format: ['esm'],
+  shims: true,
+  clean: true,
+  dts: { resolve: false },
+  treeshake: true,
+  minify: true,
+});
