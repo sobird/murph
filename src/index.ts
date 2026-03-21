b90d7fb8a776b18d59df8c5053daacc1c0dd78b3
sobird <i@sobird.me>
2026-03-21 21:19:02 +0800

    feat: upgrade workflows

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @sobird/void
+ *
+ * A pristine TypeScript template for rapid project inception.
+ */
+
+import { version } from '../package.json';
+
+/**
+ * The core inception point of the application.
+ */
+export function inception() {
+  // eslint-disable-next-line no-console
+  console.log('Project initialized in the void.', version);
+}
