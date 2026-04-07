2db4e31c0555237b9771dc92df9011d5425036fd
sobird <i@sobird.me>
2026-04-08 07:42:18 +0800

    fix: unit test error

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,12 +2,13 @@ import {
   describe, expect, it, vi,
 } from 'vitest';
 
+import { description, name, version } from '../package.json';
 import { murph } from './index';
 
 describe('murph function', () => {
   it('should log the correct message', () => {
     const consoleSpy = vi.spyOn(console, 'log');
     murph();
-    expect(consoleSpy).toHaveBeenCalledWith('@sobird/murph(1.3.1)', 'A pristine TypeScript template for rapid project inception.');
+    expect(consoleSpy).toHaveBeenCalledWith(`${name}(${version})`, description);
   });
 });
